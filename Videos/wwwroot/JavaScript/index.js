f1f29a5fb9860ec718e52d97bb390cd0c09978cb
JessicaEscamilla <jescamilla02@unisalle.edu.co>
2018-06-13 18:08:45 +0200

    Practicando jquery P1

--- Videos/wwwroot/JavaScript/index.js
+++ Videos/wwwroot/JavaScript/index.js
@@ -1,16 +1,20 @@
-﻿var x = 0;
-var s = "";
+﻿$Cdocument).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("Mi primer Java script");
+    console.log("Mi primer Java script");
 
-var theForm = document.getElementById("theform");
-theForm.hidden = true;
+    var theForm = $("#theform");
+    theForm.hide();
 
-var button = document.getElementById("BuyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
-})
-var productInfo = document.getElementByClassName("Propiedades");
-var ListItems = productInfo.item[0].children;
+    var button = $("#BuyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    })
+    var productInfo = $(".Propiedades li");
+    productInfo.on("click", function () {
+        console.log("you clicked on " + $(this).text());
+    })
 
+});
 
